cfd42749a78d9ab399a33f9affd0534ae53d830f
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2024-03-18 11:02:16 +0530

    18-03-24

---
+++ html/action.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$email = 'keyur';
+$pswd =14;
+
+if ($email=='keyur12@gmail.com' && $pswd == 14) {
+  echo "welcome".$email;
+}
+else{
+ echo "sign in";
+}
+?>
+ 
+</body>
+</html>
